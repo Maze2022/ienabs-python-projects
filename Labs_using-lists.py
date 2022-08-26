218399c0bbfefeb9716984d8dac0efadd8da0d79
Imaze Enabulele <ienabulele84@gmail.com>
2022-08-26 08:31:51 +0000

    reversed the user list

--- Labs_using-lists.py
+++ Labs_using-lists.py
@@ -16,7 +16,18 @@ del users[1]
 assert users == ['kevin', 'alice'], f"Expected 'users' to be ['kevin', 'alice'] but got: {repr(users)}"
 
 # 4) Reverse the users list and assign the result to the 'rev_users'
+rev_users = list(reversed(users))
 
 assert rev_users == ['alice', 'kevin'], f"Expected 'rev_users' to be ['alice', 'kevin'] but got: {repr(rev_users)}"
 
 # 5) Add user 'melody' to users where 'bob' used to be.
+
+assert rev_users == ['kevin', 'melody', 'alice'], f"Expected 'users' to be ['kevin', 'melody', 'alice'] but got: {repr(users)}"
+
+# 6) Add the users 'andy', 'wanda', and 'jim' to the users list using a single command
+
+assert rev_users == ['kevin', 'melody', 'alice', 'andy', 'wanda', 'jim'], f"Expected 'users' to be ['kevin', 'melody', 'alice', 'andy', 'wanda', 'jim'] but got: {repr(rev_users)}"
+
+#7) Slice the users lists to return the 3rd and 4th items and assign the result to 'center_users'
+
+assert center_users == ['alice', 'andy'], f"Expected 'users' to be ['alice', 'andy'] but got: {repr(rev_users)}"
