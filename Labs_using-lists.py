c83fe0d62445f8eb288ef5e3a886ceb70be5df3d
Imaze Enabulele <ienabulele84@gmail.com>
2022-08-26 07:54:42 +0000

    Python list of users

---
+++ Labs_using-lists.py
@@ -0,0 +1,17 @@
+# 1) Set the users variable to be an empty list
+
+assert users == [], f"Expected 'users' to be [] but got: {repr(users)}"
+
+# 2) Add 'kevin', 'bob', and 'alice' to the users list in that order without reassigning the variable.
+
+assert users == ['kevin', 'bob', 'alice'], f"Expected 'users' to be ['kevin', 'bob', 'alice'] but got: {repr(users)}"
+
+# 3) Remove 'bob' from the users list without reassigning  the variable.
+
+assert users == ['kevin', 'alice'], f"Expected 'users to be ['kevin', 'alice'] but got: {repr(users)}"
+
+# 4) Reverse the users list and assign the result to the 'rev_users'
+
+assert rev_users == ['alice', 'kevin'], f"Expected 'rev_users to be ['alice', 'kevin'] but got: {repr(users)}"
+
+# 5) Add user 'melody' to users where 'bob' used to be.
