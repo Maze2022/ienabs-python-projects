2a14b0612efed48ed0fe599f3e5ad7abadd98fb3
Imaze Enabulele <ienabulele84@gmail.com>
2022-08-27 14:30:08 +0000

    Polulated the list with AWS services

--- AWS-Services-Inventory.py
+++ AWS-Services-Inventory.py
@@ -1,4 +1,15 @@
-# ) Create an empty AWS Services list
+# 1) Create an empty AWS Services list
 AWS_serv_list = [ ]
+
+# 2) Populate the list with AWS services
+AWS_serv_list.insert(0, "EC2")
+AWS_serv_list.insert(1,"Lambda")
+AWS_serv_list.insert(2, "Elastic Beanstalk")
+AWS_serv_list.insert(3, "SQS")
+AWS_serv_list.insert(4, "CloudTrail")
+AWS_serv_list.insert(5, "DynamoDB")
+AWS_serv_list.insert(6, "SNS")
+AWS_serv_list.insert(7, "Lightsail")
+AWS_serv_list.insert(8, "S3")
 
 
