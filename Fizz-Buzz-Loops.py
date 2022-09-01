b0b167292184d949066931798c5de4329044c36e
Imaze Enabulele <ienabulele84@gmail.com>
2022-09-01 06:28:53 +0000

    Created range to users provided number

---
+++ Fizz-Buzz-Loops.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.7
+
+# Python implementation here
+
+upper_number = int(input("How many values should we process: "))
+
+for number in range(1, upper_number + 1):
+    print(number)
