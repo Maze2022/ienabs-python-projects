0af828efd28e85e46805ba7103d72b99f281c444
Imaze Enabulele <ienabulele84@gmail.com>
2022-09-01 05:09:48 +0000

    Email variable set to an empty dictionary

--- using-dictionaries.py
+++ using-dictionaries.py
@@ -5,10 +5,6 @@ assert emails == {}, f"Expected `emails` to be {{}} but got: {repr(emails)}"
 
 # 2) Add 'ashley', 'craig', and 'elizabeth' to the emails dictionary without reassigning the variable.
 
-emails['ashley'] = 'ashley@example.com'
-emails['craig'] = 'craig@example.com'
-emails['elizabeth'] = 'elizabeth@example.com'
-
 assert emails == {
     "ashley": "ashley@example.com",
     "craig": "craig@example.com",
@@ -17,15 +13,12 @@ assert emails == {
 
 # 3) Remove 'craig' from the emails dictionary without reassigning the variable.
 
-del emails['craig']
-
 assert emails == {
     "ashley": "ashley@example.com",
     "elizabeth": "elizabeth@example.com",
 }, f"Expected `emails` to be {{'ashley': 'ashley@example.com', 'elizabeth': 'elizabeth@example.com'}} but got: {repr(emails)}"
 
 # 4) Add 'dalton' to the emails dictionary without reassigning the variable.
-emails['dalton'] = 'dalton@example.com'
 
 assert emails == {
     "ashley": "ashley@example.com",
@@ -34,7 +27,6 @@ assert emails == {
 }, f"Expected `emails` to be {{'ashley': 'ashley@example.com', 'elizabeth': 'elizabeth@example.com', 'dalton': 'dalton@example.com'}} but got: {repr(emails)}"
 
 # 5) Return a list of keys from the emails dictionary as `users`
-users = list(emails.keys())
 
 assert users == [
     "ashley",
@@ -43,7 +35,6 @@ assert users == [
 ], f"Expected `users` to be ['ashley', 'elizabeth', 'dalton'] but got: {repr(users)}"
 
 # 6) Return a list of values from the emails dictionary as `email_list`
-email_list = list(emails.values())
 
 assert email_list == [
     "ashley@example.com",
@@ -52,7 +43,6 @@ assert email_list == [
 ], f"Expected `email_list` to be ['ashely@example.com', 'elizabeth@example.com', 'dalton@example.com'] but got: {repr(email_list)}"
 
 # 7) Return a list of tuples called `pairs` representing the key/value pairs in `emails`.
-pairs = list(emails.items())
 
 assert pairs == [
     ("ashley", "ashley@example.com"),
